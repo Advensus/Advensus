bdd41251f4cbbe096132e6d6e01a4943c9b14fc9
christ <christikonga83@gmail.com>
2022-04-04 18:46:44 +0200

    update databases login

--- compte/models.py
+++ compte/models.py
@@ -1,4 +1,14 @@
+from audioop import maxpp
 from django.db import models
+
+class Organisme(models.Model):
+    company_name = models.CharField(max_length=100)
+    company_adress = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    fix_number = models.CharField(max_length=50)
+    company_stamp = models.FileField(upload_to="company_stamp/")
+    company_logo = models.FileField(upload_to="company_logo/")
+
 
 
 # Create your models here.
