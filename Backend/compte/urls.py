398b7f9206ffbb7aa22a014e5e638aadd5c6c6c6
christ <christikonga83@gmail.com>
2022-04-12 17:33:36 +0200

    all

--- Backend/compte/urls.py
+++ Backend/compte/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.home,name="home")
+    path('', views.home,name="home"),
+    path('auth/', include('rest_auth.urls')),    
+    path('auth/register/', include('rest_auth.registration.urls'))
+    # path('register/', views.register,name="home")
 ]
--- Backend/compte/views.py
+++ Backend/compte/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from .user import User
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def home(request):
-    return HttpResponse("<h1>Adavance Formation</h1>")
+	api_urls = {
+		'register':'register/',
+		# 'Detail Vue':'/task-detail/<str:pk>/',
+		# 'Create':'/task-create/',
+		# 'Update':'/task-update/<str:pk>/',
+		# 'Delete':'/task-delete/<str:pk>/',
+		}
+
+	return Response(api_urls)
+
+   
+
+# @api_view(['POST'])
+# def register(request):
+#     if method == POST:
+#         first_name = request.POSt.get('frist_name')
+#         email = request.POST.get('email')
+#         password1 = request.POST.get('password1')
+#         password2 = request.POST.get('password2')
+
+#         if password1 == password2:
+#             if User.objects.filter(email=email).exists():
+#                 messages.error(request, "mail existant")
+#             else: 
+#                 user = User.objects.create_user(first_name=first_name,email=email)
+#                 user.save()
+#                 print("okkk")
+#         elif password1 != password2:
+#             messages.error(request,"different")
+        
+#     else:
+#         print("accueil")
+
+  
 # Create your views here.
