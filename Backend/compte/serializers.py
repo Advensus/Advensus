fd63c6d3a6bfa1d203ae73ea8036ffe942ca91b1
christ <christikonga83@gmail.com>
2022-04-13 16:55:17 +0200

    all

--- Backend/compte/serializers.py
+++ Backend/compte/serializers.py
@@ -7,8 +7,8 @@ from .user import User
 class AddStagiaire(serializers.ModelSerializer):
     password= serializers.CharField(max_length=60, min_length=8,write_only=True)
     class Meta:
-        model = Stagiaire
-        fields = ('username','first_name','email','phone_number','Adress','is_client','password')
+        model = User
+        fields = ['username','first_name','email','phone_number','Adress','is_client','password']
 
     def validate(self,attrs):
         email = attrs.get('email','')
@@ -17,32 +17,32 @@ class AddStagiaire(serializers.ModelSerializer):
         phone_number = attrs.get('phone_number','')
         Adress = attrs.get('Adress','')
             
-        if username.isalnum():
+        if not username.isalnum():
             raise serializers.ValidationError('Le nom ne peut contenir que des caractère alphanumerique')
                 
-        elif first_name.isalnum():
+        elif not first_name.isalnum():
             raise serializers.ValidationError('Le prenom ne peut contenir que des caractère alphanumerique')
         return attrs
     def create(self,validated_data):
         return User.objects.create_user(**validated_data)
 
-class AddRp(serializers.ModelSerializer):
-    class Meta:
-        model = responsable_p
-        fields = ('username','first_name','email','phone_number','Adress',' is_planificateur','password')
+# class AddRp(serializers.ModelSerializer):
+#     class Meta:
+#         model = responsable_p
+#         fields = ('username','first_name','email','phone_number','Adress',' is_planificateur','password')
 
-class AddSrp(serializers.ModelSerializer):
-    class Meta:
-        model = super_p
-        fields = ('username','first_name','email','phone_number','Adress','is_sup_planificateur','password')
+# class AddSrp(serializers.ModelSerializer):
+#     class Meta:
+#         model = super_p
+#         fields = ('username','first_name','email','phone_number','Adress','is_sup_planificateur','password')
 
-class AddOrg(serializers.ModelSerializer):
-    class Meta:
-        model = Organisme
-        fields = ('company_name','company_adress','phone_number','fix_number','is_organisme')
+# class AddOrg(serializers.ModelSerializer):
+#     class Meta:
+#         model = Organisme
+#         fields = ('company_name','company_adress','phone_number','fix_number','is_organisme')
 
-class AddFormateur(serializers.ModelSerializer):
-    class Meta:
-        model = Formateur
-        fields = ('username','first_name','email','phone_number','Adress','is_sup_planificateur','password','horaire','competence','cv')
+# class AddFormateur(serializers.ModelSerializer):
+#     class Meta:
+#         model = Formateur
+#         fields = ('username','first_name','email','phone_number','Adress','is_sup_planificateur','password','horaire','competence','cv')
 
