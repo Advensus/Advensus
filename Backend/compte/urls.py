7f554bb2da52483428d5a30f9fc9637dc8ba0b0f
christ <christikonga83@gmail.com>
2022-04-14 15:44:16 +0200

    backend

--- Backend/compte/urls.py
+++ Backend/compte/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,include
-from .views import RegisterStagiaire
+from .views import RegisterStagiaire,VerifyEmail
 from . import views
 
 urlpatterns = [
     path('', views.home,name="home"),
-    path('register/stagiaire/', RegisterStagiaire.as_view(), name="register"),    
+    path('register/stagiaire/', RegisterStagiaire.as_view(), name="register"),
+    path('email-verify/', VerifyEmail.as_view(), name="email-verify"),       
     # path('auth/register/', include('rest_auth.registration.urls'))
     # path('register/', views.register,name="home")
 ]
--- Backend/compte/views.py
+++ Backend/compte/views.py
@@ -5,7 +5,9 @@ from .serializers import AddStagiaire
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .user import User
-
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
 def home(request):
 	return HttpResponse("<h1>Advensus projet</h1>")
 
@@ -18,7 +20,21 @@ class RegisterStagiaire(generics.GenericAPIView):
 		serializer.is_valid(raise_exception=True)
 		serializer.save()
 		user_data = serializer.data
+
 		user = User.objects.get(email=user_data['email'])
-		token = RefreshToken.for_user(user)
+		token = RefreshToken.for_user(user).access_token
+
+		current_site = get_current_site(request).domain
+		relativelink= reverse('email-verify')
+		absurl = 'http://'+current_site+relativelink+"?token="+str(token)
+		email_body = 'salut '+user.username+'utilise ce lien pour verifier ton compte\n'+absurl
+		
+		data = {'email_body': email_body,'to_email': user.email,'email_subject': 'verifier votre adress email'+current_site}
+		Util.send_email(data)
 
 		return Response(user_data,status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(generics.GenericAPIView):
+	def get(self):
+		pass
