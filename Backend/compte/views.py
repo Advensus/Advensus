df806c314e1fd9599350a900e134f74c95ee109a
christ <christikonga83@gmail.com>
2022-04-14 14:42:38 +0200

    backend

--- Backend/compte/views.py
+++ Backend/compte/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from rest_framework import generics,status
 from .serializers import AddStagiaire
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .user import User
 
 def home(request):
 	return HttpResponse("<h1>Advensus projet</h1>")
@@ -16,5 +18,7 @@ class RegisterStagiaire(generics.GenericAPIView):
 		serializer.is_valid(raise_exception=True)
 		serializer.save()
 		user_data = serializer.data
+		user = User.objects.get(email=user_data['email'])
+		token = RefreshToken.for_user(user)
 
 		return Response(user_data,status=status.HTTP_201_CREATED)
