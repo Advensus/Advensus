b07eb3bb63b2d6ac9f40f8958e6bc420caf7963c
christ <christikonga83@gmail.com>
2022-04-25 17:44:14 +0200

    user logout view

--- Backend/Advensus/settings.py
+++ Backend/Advensus/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -180,7 +181,10 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.IsAuthenticated',
     # )
 }
-
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':datetime.timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME':datetime.timedelta(days=1)
+}
 SWAGGER_SETTINGS ={
     'SECURITY_DEFINITIONS':{
         'Bearer':{
