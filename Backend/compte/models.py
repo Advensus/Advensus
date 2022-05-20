d35d4e4a74606cd51af1312b58e58ed60fb82046
christ <christikonga83@gmail.com>
2022-05-20 18:08:27 +0200

    reservation

--- Backend/compte/models.py
+++ Backend/compte/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from .utilisateur import User
 from .training import formation
 import uuid
+from django.conf import settings
 # class souscriptionliaison(models.Model):
    
 #     utilisateur = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -13,22 +14,34 @@ import uuid
     # souscriptionliaison = models.ManyToManyField(souscriptionliaison,related_name='souscription_content_type')
 
 
-
 class Presence(models.Model):
     id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     stagiaireform = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+class reservation(models.Model):
+    id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=255)
+    reserver = models.ManyToManyField(User,related_name='reservation_content_type')
+    status = models.CharField(max_length=30)
+    annuler = models.BooleanField(default=False)
+    start_date = models.DateField(auto_now_add=False)
+    end_date = models.DateField(auto_now_add=False)
+    proposer = models.ForeignKey(User,on_delete=models.CASCADE,related_name='proposer_content_type')
+  
+    concerner = models.OneToOneField(settings.COURSES,on_delete=models.CASCADE)
+   
+class Courses(models.Model):
+    superviser = models.ForeignKey(User,on_delete=models.CASCADE,related_name='superviser_content_type')
+    assister = models.ManyToManyField(User,related_name='assister_content_type')
+    etablir = models.OneToOneField(Presence,on_delete=models.CASCADE)
+    reservation_cours = models.OneToOneField(reservation,on_delete=models.CASCADE)
+    lier = models.ForeignKey(formation,on_delete=models.CASCADE)
+
     # formateur = models.ForeignKey(Formateur,on_delete=models.CASCADE)
     
 
-class reservation(models.Model):
-    id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    reservation = models.ManyToManyField(User,related_name='reservation_content_type')
-    status = models.CharField(max_length=30)
-    annuler = models.BooleanField(default=False)
-    proposer = models.ForeignKey(User,on_delete=models.CASCADE,related_name='proposer_content_type')
-  
-    etablir = models.OneToOneField(Presence,on_delete=models.CASCADE)
-   
+
 
 class Document(models.Model):
     id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -38,12 +51,6 @@ class Document(models.Model):
     partager = models.ForeignKey(User,on_delete=models.CASCADE,related_name='partager_content_type')
     emarger = models.ForeignKey(User,on_delete=models.CASCADE,related_name='emarger_content_type')
 
-class Courses(models.Model):
-    superviser = models.ForeignKey(User,on_delete=models.CASCADE,related_name='superviser_content_type')
-    assister = models.ManyToManyField(User,related_name='assister_content_type')
-    etablir = models.OneToOneField(Presence,on_delete=models.CASCADE)
-    reservation_cours = models.OneToOneField(reservation,on_delete=models.CASCADE)
-    lier = models.ForeignKey(formation,on_delete=models.CASCADE)
 
 class Opinion(models.Model):
     rapporter = models.ForeignKey(Courses,on_delete=models.CASCADE)
