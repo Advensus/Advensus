8a457cf088f8b82f02ec4dfd3c3574b6138790c8
christ <christikonga83@gmail.com>
2022-05-30 12:28:19 +0200

    create table certificate with programme relationship

--- Backend/compte/training.py
+++ Backend/compte/training.py
@@ -22,3 +22,9 @@ class certificate(models.Model):
     competence_atteste = models.CharField(max_length=100)
     modalite_evaluation = models.CharField(max_length=100)
     allouer = models.ManyToManyField(formation)
+
+class programme(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,unique=True)
+    intitule = models.CharField(max_length=100)
+    description = models.TextField(max_length=255)
+    attribue = models.ForeignKey(certificate,on_delete=models.CASCADE)
