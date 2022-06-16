9b4ea65a0011af07033c9e6388edd2f82f516316
Rolkyssoh <juventussoh@gmail.com>
2022-06-16 12:40:59 +0100

    some changed

--- Backend/Advensus/settings.py
+++ Backend/Advensus/settings.py
@@ -208,8 +208,8 @@ EMAIL_USE_TLS: True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 # MEDIA_ROOT =  BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
 MEDIA_URL = ''
