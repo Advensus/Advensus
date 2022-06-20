8bb6f1e2947895402661d0a0b17077d174a719ac
christ <christikonga83@gmail.com>
2022-06-20 15:50:42 +0200

    create_at delete_at update_at

--- Backend/compte/company.py
+++ Backend/compte/company.py
@@ -36,6 +36,9 @@ class SocieteFormation(Company):
     #     Company,
     #     on_delete=models.CASCADE,
     # )
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    delete_at = models.DateTimeField(auto_now=True)
     
 
 
